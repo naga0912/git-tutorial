415490370fbeeaaf2b4af044a953cc3ef0b106e4
nagasekosei <j2022@matsue-ct.ac.jp>
2023-11-30 15:52:03 +0900

    second commit

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,6 +1,29 @@
-
-#include<stdio.h>
-
-int main(void){
- int x;
+#include <stdio.h>
+int main(void)
+{
+  int i;
+  for (i = 0; i <= 100; i++)
+  {
+    int FizzBuzz = i % 15;
+    int Fizz = i % 3;
+    int Buzz = i % 5;
+    if (FizzBuzz == 0 && i != 0)
+    {
+      printf("FizzBuzz: %d\n", i);
+    }
+    else if (Fizz == 0 && i != 0)
+    {
+      printf("Fizz: %d\n", i);
+    }
+    else if (Buzz == 0 && i != 0)
+    {
+      printf("Buzz: %d\n", i);
+    }
+    else
+    {
+      printf("i: %d\n", i);
+    }
+  }
+  return 0;
 }
+
